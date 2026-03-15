31455c2079cb39b24c67ae146b725d931fb00391
PriyankaRathod <priyanka.rathod3020@gmail.com>
2026-03-15 22:29:43 +0530

    commiting the variable file

--- variable_prj/main.tf
+++ variable_prj/main.tf
@@ -2,3 +2,45 @@ resource "local_file" "variable" {
  filename = var.filename
  content = var.content
 }
+
+resource "random_string" "rstring" {
+ length = 16
+ special = true
+ override_special = "*()<>?"
+}
+
+output rstring {
+ value= random_string.rstring.result
+}
+
+
+output "output_env_var"{
+ value=var.env_var
+}
+
+output "output_map1"{
+ value=var.map_var.statement1
+}
+
+output "output_map2"{
+ value=var.map_var.statement2
+}
+
+output "output_object"{
+ value={
+  name=var.object_var.name
+  count=var.object_var.count
+  count_fruits=var.object_var.count_fruits
+  fruits=var.object_var.fruits
+  dictionary1=var.object_var.dictionary.w1
+  dictionary2=var.object_var.dictionary.w2
+ }
+}
+
+
+output "json_var"{
+value={
+ msg=var.msg
+ count=var.no_of
+}
+}
--- variable_prj/variable.tf
+++ variable_prj/variable.tf
@@ -1,7 +1,47 @@
 variable "filename" {
-  default= "/local_dir/test.txt"
+  default= "/home/pikalinux/terraform/variables_prj/test.txt"
 }
 
 variable "content" {
  default= "This is a variable terraform file"
 }
+
+# In below line we are declaring the variable and will be giving its value by export TF_VAR command as a environment variable
+// command to assign value to the variable - export TF_VAR_variable_name=value (eg. export TF_VAR_env_var=abc)
+variable "env_var" {}
+
+#variable of type map
+variable "map_var"{
+type=map
+default={
+ "statement1"="This is 1st statement"
+ "statement2"="This is 2nd statement"
+}
+}
+
+variable "object_var"{
+ type=object({
+ name=string
+ count=number
+ count_fruits=list(number)
+ fruits=set(string)
+ dictionary=map(string)
+})
+
+default={
+ name="Alice"
+ count=2
+ count_fruits=[100,200,300]
+ fruits=["apple","kiwi","orange"]
+ dictionary={
+  "w1"="map word1"
+  "w2"="map word2"
+ }
+}
+}
+
+// below variables declaration is for accessing the variable and its value from json file. the variable name should be same as the key in json file
+// to run the terraform using json file - [terraform apply -var-file=filename.tfvars.json]. to create a var file the file should have extension ".tfvars" 
+variable "msg" {}
+
+variable "no_of" {}
